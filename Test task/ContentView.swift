9ffb1f9f3b9502e1331e3bf67b2d12314296baab
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2021-03-19 00:37:42 +0500

    Initial Commit

---
+++ Test task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test task
+//
+//  Created by Сергей on 19.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test task/Test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_taskApp.swift
+//  Test task
+//
+//  Created by Сергей on 19.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
